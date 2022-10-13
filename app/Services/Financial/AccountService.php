e74ea4112fbaeb29e53ae006c2350af2e51c01a1
Pouriya Khazaei <pouriya.khazaei@arvancloud.com>
2022-10-13 22:32:11 +0330

    use account repository in check access method

--- app/Services/Financial/AccountService.php
+++ app/Services/Financial/AccountService.php
@@ -9,7 +9,7 @@ class AccountService
 {
     public static function checkAccess(string $id, string $userID): bool
     {
-        return Account::query()->where('user_id', $userID)->where('id', $id)->count() == 1;
+        return AccountRepository::checkAccess($id, $userID);
     }
 
     public static function changeBalance(Account &$account, int $amount): int
