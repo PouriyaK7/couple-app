dfb4f1210a8aec17c7d0fd3d4c34eb9082627498
Pouriya Khazaei <pouriya.khazaei@arvancloud.com>
2022-10-13 16:31:28 +0330

    added account repository

--- app/Repositories/Financial/AccountRepository.php
+++ app/Repositories/Financial/AccountRepository.php
@@ -2,6 +2,45 @@
 
 namespace App\Repositories\Financial;
 
+use App\Models\Account;
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Support\Str;
+
 class AccountRepository
 {
+    public static function store(string $title, string $bank, string $userID, string $description = null): Account
+    {
+        return Account::query()->create([
+            'id' => Str::uuid()->toString(),
+            'title' => $title,
+            'bank' => $bank,
+            'user_id' => $userID,
+            'description' => $description
+        ]);
+    }
+
+    public static function update(Account &$account, string $title, string $bank, string $description = null): void
+    {
+        $account->title = $title;
+        $account->bank = $bank;
+        $account->description = $description;
+        $account->save();
+    }
+
+    public static function delete(string $id): bool
+    {
+        return Account::query()->where('id', $id)->delete();
+    }
+
+    public static function sumBalance(Account &$account, int $amount): int
+    {
+        $account->balance += $amount;
+        $account->save();
+        return $account->balance;
+    }
+
+    public static function transactions(string $id): Collection
+    {
+        return Account::query()->where('id', $id)->transactions;
+    }
 }
