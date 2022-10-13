d4b31d7baa168030ff75b761a2f5a635515afad8
Pouriya Khazaei <pouriya.khazaei@arvancloud.com>
2022-10-13 22:34:13 +0330

    removed sum balance method from account service

--- app/Services/Financial/AccountService.php
+++ app/Services/Financial/AccountService.php
@@ -2,7 +2,6 @@
 
 namespace App\Services\Financial;
 
-use App\Models\Account;
 use App\Repositories\Financial\AccountRepository;
 
 class AccountService
@@ -10,10 +9,5 @@ class AccountService
     public static function checkAccess(string $id, string $userID): bool
     {
         return AccountRepository::checkAccess($id, $userID);
-    }
-
-    public static function changeBalance(Account &$account, int $amount): int
-    {
-        return AccountRepository::sumBalance($account, $amount);
     }
 }
