bd460573e20a252ead81b99decd3fb14051549fb
Pouriya Khazaei <pouriya.khazaei@arvancloud.com>
2022-10-14 21:25:07 +0330

    completed couple user model

--- app/Models/CoupleUser.php
+++ app/Models/CoupleUser.php
@@ -4,8 +4,22 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class CoupleUser extends Model
 {
     use HasFactory;
+
+    public $incrementing = false;
+    protected $guarded = [];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function couple(): BelongsTo
+    {
+        return $this->belongsTo(Couple::class);
+    }
 }
