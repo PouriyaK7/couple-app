80736152b437370557838e275e093e3d03e2d573
Pouriya Khazaei <pouriya.khazaei@arvancloud.com>
2022-10-14 21:01:33 +0330

    removed incrementing from user model

--- app/Models/User.php
+++ app/Models/User.php
@@ -14,6 +14,7 @@ class User extends Authenticatable
     use HasApiTokens, HasFactory, Notifiable;
 
     protected $guarded = [];
+    public $incrementing = false;
 
     /**
      * The attributes that should be hidden for serialization.
